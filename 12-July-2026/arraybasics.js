b55af6f2a357ca9f157b573f6ca2fb4e2d00af8b
0033 <avachmansaxena@gmail.com>
2026-07-24 11:04:02 +0530

    modified:   12-July-2026/arraybasics.js

--- 12-July-2026/arraybasics.js
+++ 12-July-2026/arraybasics.js
@@ -9,6 +9,6 @@ console.log(`Third value of the array is ${testScores[2]}`);
 let thirdValueOfArray = testScores[2];
 console.log("No. of items in the list is - " + testScores.length); //5
 
-console.log(`Guess ${testScores[-1]}`);
+console.log(`Guess ${testScores[-1]}`); // 
 
 console.log("Last value of the array - " + testScores[testScores.length - 1]);
