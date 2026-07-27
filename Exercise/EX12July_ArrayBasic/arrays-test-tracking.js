a3c1cd27b570c86792cfbd554aed23c10d4f9b05
0033 <avachmansaxena@gmail.com>
2026-07-27 12:02:24 +0530

    modified:   Exercise/EX12July_ArrayBasic/arrays-test-tracking.js

--- Exercise/EX12July_ArrayBasic/arrays-test-tracking.js
+++ Exercise/EX12July_ArrayBasic/arrays-test-tracking.js
@@ -35,10 +35,52 @@ console.log(`Full array is : [${testResults}]`);
 console.log("Index of  FAILED is : "+testResults.indexOf("FAILED"));
 
 console.log("\n----------------Test Data Management :-----------------\n");
-let testEmails = [ "user1@test.com" , "user2@test.com" , "user3@test.com" ];
+
+let testEmails = [ '"user1@test.com"' , '"user2@test.com"' , '"user3@test.com"' ];
 
 console.log("Test Email Management");
 console.log(`Initial email : [${testEmails}] `);
-console.log("Initial email : ["+testEmails+"]");
-console.log("\n----------------Test Data Management :-----------------\n");
+console.log("Count of element : "+testEmails.length);
+
+testEmails[3]= '"user4@test.com"';
+console.log(`Updated email :\n [${testEmails}] `); //After adding user4@test.com to end:
+console.log("Count of element : "+testEmails.length +"\n");
+
+testEmails.push('"achman"');//adds value at the end of ARRAY;
+console.log(`add at the end again using method :\n [${testEmails}] `);
+console.log("Count of element : "+testEmails.length +"\n");
+
+testEmails.unshift('"admin@test.com"');//adds value at the beginning of ARRAY;
+console.log(`add at the end again using method :\n [${testEmails}] `);
+console.log("Count of element : "+testEmails.length +"\n");
+
+let removed = testEmails.pop();//remove value at the end of ARRAY;
+console.log(`Removed from the end again using method :\n [${testEmails}] `);
+console.log("Count of element : "+testEmails.length +"\n");
+console.log("element removed : "+removed);
+
+removed = testEmails.shift();//Remove value at the beginning of ARRAY;
+console.log(`Removed from the end again using method :\n [${testEmails}] `);
+console.log("Count of element : "+testEmails.length +"\n");
+console.log("element removed : "+removed);
+
+
+console.log("\n----------------Test Score Analysis :-----------------\n");
+
+testScores = [85, 92, 78, 95, 88, 73, 90];
+console.log(testScores);
+console.log(`Total number of score is : ${testScores.length}`);
+console.log(`First score is : ${testScores[0]}`);
+console.log(`Last score is : ${testScores[testScores.length-1]}`);
+console.log("Checking for specific scores...");
+let c ;
+function indexoffelement (c){
+if(testScores.indexOf(c) !== -1)
+   console.log(`Score ${c} found : Yes (at index: ${testScores.indexOf(c)})`);
+else 
+   console.log(`Score ${c} found : NO (at index: ${testScores.indexOf(c)})`);
+}
+indexoffelement(95);
+indexoffelement(2);
+
 
